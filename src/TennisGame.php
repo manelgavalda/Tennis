5dc806fc599dcf8b94a5b0cdc4c4449537d4fd9e
manel <manelgavalda@iesebre.com>
2016-10-25 17:42:29 +0200

    Testing

--- src/TennisGame.php
+++ src/TennisGame.php
@@ -17,25 +17,45 @@ class TennisGame
 
     public function score(){
 
-        if($this->player1->score()>3 || $this->player1->score()>3){
-            $diferencia= $this->player1->points() - $this->player2->points();
-            if($diferencia>0){
-                //guanya player1
-                return "Avatantatge". $this->player1->name();
-            }
-            elseif ($diferencia==0){
-                //Iguals
-                return "Iguals";
-            }
-            elseif ($diferencia<0){
-                //player 2 guanya
-                return "Avatantatge". $this->player1->name();
-            }
+        if($this->tie()){
+            return $this->resolveTie();
         }
 
         if ($this->player1->score() == $this->player2->score()){
             return $this->player1->score() ." iguals";
         }
         return $this->player1->score()." - ".$this->player2->score();
+    }
+
+    protected function resolveTie(){
+        $diferencia= $this->player1->points() - $this->player2->points();
+
+        if ($diferencia==0){
+            return "Iguals";
+        }
+            $playerwins = $this->player1;
+
+
+            if ($diferencia<0) {
+                //player 2 guanya
+                $playerwins = $this->player2;
+            }
+        return $this->scoreAvantatge($playerwins);
+
+    }
+    /**
+     * @return bool
+     */
+    public function tie()
+    {
+        return $this->player1->points() > 3 || $this->player1->points() > 3;
+    }
+
+    /**
+     * @return string
+     */
+    protected function scoreAvantatge($player)
+    {
+        return "Avantatge " . $player->name();
     }
 }
